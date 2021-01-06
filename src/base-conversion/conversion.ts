48402c1957ceffa4295d053a771752d33e3ad1e2
iwfan <i.wangfancn@gmail.com>
2021-01-06 22:25:10 +0800

    update base conversion

--- src/base-conversion/conversion.ts
+++ src/base-conversion/conversion.ts
@@ -1,9 +1,28 @@
-export function dicimal2binary(dicimalNumber: number): number {
+export const dicimal2binary = (dicimalNumber: number): string => {
+  // return Number.prototype.toString.call(dicimalNumber, 2)
+
   let result = "";
 
-  for (let n = dicimalNumber; n > 0; n / 2) {
+  /**
+    JS 中的数字都是浮点数类型，所以如果for循环最后一个语句写为 n /= 2, 结果就是不对的。
+    但是用右移就不会有这样的问题。第二种方式就是 parseInt
+  */
+  for (let n = dicimalNumber; n > 0; n = n >> 1) {
     result = (n % 2) + result;
   }
 
-  return Number(result);
+  return result;
+}
+
+export const binary2dicimal = (binaryNumber: string): number => {
+  let dicimalNumber = 0
+
+  for (let i = 0; i < binaryNumber.length; i++) {
+    const binaryNum = Number(binaryNumber.charAt(i))
+    // dicimalNumber += binaryNum * Math.pow(2, binaryNumber.length - 1 - i);
+    // a ** b, same as Math.pow(a, b)
+    dicimalNumber += binaryNum * 2 ** (binaryNumber.length - 1 - i);
+  }
+
+  return dicimalNumber;
 }
--- src/base-conversion/conversion_test.ts
+++ src/base-conversion/conversion_test.ts
@@ -1,6 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts"
-import { dicimal2binary } from "./conversion.ts"
+import { binary2dicimal, dicimal2binary } from "./conversion.ts"
 
-Deno.test("dicimal to binart", () => {
-  assertEquals(dicimal2binary(2), 10);
+Deno.test("dicimal to binary", () => {
+  assertEquals(dicimal2binary(53), '110101');
 });
+
+Deno.test("binary to dicimal", () => {
+  assertEquals(binary2dicimal('110101'), 53)
+})

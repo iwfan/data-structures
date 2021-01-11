1ff58da890f0a05c75494f285e8e87581903ba6d
iwfan <i.wangfancn@gmail.com>
2021-01-11 08:31:52 +0800

    fix typo

--- src/base-conversion/conversion.ts
+++ src/base-conversion/conversion.ts
@@ -1,5 +1,5 @@
-export const dicimal2binary = (dicimalNumber: number): string => {
-  // return Number.prototype.toString.call(dicimalNumber, 2)
+export const decimal2binary = (decimalNumber: number): string => {
+  // return Number.prototype.toString.call(decimalNumber, 2)
 
   let result = "";
 
@@ -7,22 +7,22 @@ export const dicimal2binary = (dicimalNumber: number): string => {
     JS 中的数字都是浮点数类型，所以如果for循环最后一个语句写为 n /= 2, 结果就是不对的。
     但是用右移就不会有这样的问题。第二种方式就是 parseInt
   */
-  for (let n = dicimalNumber; n > 0; n = n >> 1) {
+  for (let n = decimalNumber; n > 0; n = n >> 1) {
     result = (n % 2) + result;
   }
 
   return result;
 }
 
-export const binary2dicimal = (binaryNumber: string): number => {
-  let dicimalNumber = 0
+export const binary2decimal = (binaryNumber: string): number => {
+  let decimalNumber = 0
 
   for (let i = 0; i < binaryNumber.length; i++) {
     const binaryNum = Number(binaryNumber.charAt(i))
-    // dicimalNumber += binaryNum * Math.pow(2, binaryNumber.length - 1 - i);
+    // decimalNumber += binaryNum * Math.pow(2, binaryNumber.length - 1 - i);
     // a ** b, same as Math.pow(a, b)
-    dicimalNumber += binaryNum * 2 ** (binaryNumber.length - 1 - i);
+    decimalNumber += binaryNum * 2 ** (binaryNumber.length - 1 - i);
   }
 
-  return dicimalNumber;
+  return decimalNumber;
 }
--- src/base-conversion/conversion_test.ts
+++ src/base-conversion/conversion_test.ts
@@ -1,10 +1,10 @@
 import { assertEquals } from "https://deno.land/std@0.83.0/testing/asserts.ts"
-import { binary2dicimal, dicimal2binary } from "./conversion.ts"
+import { binary2decimal, decimal2binary } from "./conversion.ts"
 
-Deno.test("dicimal to binary", () => {
-  assertEquals(dicimal2binary(53), '110101');
+Deno.test("decimal to binary", () => {
+  assertEquals(decimal2binary(53), '110101');
 });
 
-Deno.test("binary to dicimal", () => {
-  assertEquals(binary2dicimal('110101'), 53)
+Deno.test("binary to decimal", () => {
+  assertEquals(binary2decimal('110101'), 53)
 })
